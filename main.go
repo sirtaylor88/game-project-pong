1c0b2dd68dc0fe8cf58f75e3639b4e409fb51bb8
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-03-09 13:19:31 +0100

    Added rendering stuffs on screen.

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func printScreen(s tcell.Screen, row, col int, str string) {
+	for _, c := range str {
+		s.SetContent(col, row, c, nil, tcell.StyleDefault)
+		col++
+	}
+}
+
+func print(s tcell.Screen, row, col, width, height int, ch rune) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			s.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+		}
+	}
+}
+
+func displayHelloWorld(screen tcell.Screen) {
+	screen.Clear()
+	printScreen(screen, 1, 5, "Hello, World!")
+	print(screen, 0, 0, 5, 5, '*')
+	screen.Show()
+}
+
 func main() {
-	// Capability to render stuff on screen
+	screen := initScreen()
+
+	displayHelloWorld(screen)
+
+	for {
+		switch ev := screen.PollEvent().(type) {
+		case *tcell.EventResize:
+			screen.Sync()
+			displayHelloWorld(screen)
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape {
+				screen.Fini()
+				os.Exit(0)
+			}
+		}
+	}
+
 	// Draw paddles
 	// Player movement
 	// Take care of paddle boundaries
@@ -9,4 +54,24 @@ func main() {
 	// Update ball movement
 	// Handle collisions
 	// Handle game over
+
+}
+
+func initScreen() tcell.Screen {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+
+	return screen
 }
